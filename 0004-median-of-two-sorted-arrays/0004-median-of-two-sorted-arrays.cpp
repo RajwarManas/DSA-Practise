6ed06de1516757c83316da71f3a5996a833c3671
RajwarManas <manasrajwar.28@gmail.com>
2025-11-27 17:10:30 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0,j=0;
-        std::vector<int> vec ;
-        while(i<nums1.size()&&j<nums2.size()) {
-            if(nums1[i]<=nums2[j]) {
-                vec.push_back(nums1[i]);
-                i++;
-            }
-            else {
-                vec.push_back(nums2[j]);
-                j++;
-            }
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1, left = (n1 + n2 + 1) / 2, n = n1 + n2;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 != 0)
+                    return max(l1, l2);
+                return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
+            } else if (l1 > r2)
+                high = mid1 - 1;
+            else
+                low = mid1 + 1;
         }
-        while(i<nums1.size()) {
-            vec.push_back(nums1[i]);
-            i++;
-        }
-        while(j<nums2.size()) {
-            vec.push_back(nums2[j]);
-            j++;
-        }
-        if (vec.size()%2==0) {
-            return ((double)vec[(int)vec.size()/2]+(double)vec[(int)vec.size()/2-1])/2;
-        }
-        else return (double)vec[vec.size()/2] ;
+        return 0;
     }
 };
