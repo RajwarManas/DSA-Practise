a8e55ace4f55a79a84e0aabb81734e0f291d6024
RajwarManas <manasrajwar.28@gmail.com>
2026-03-25 20:48:02 +0530

    Time: 0 ms (100%), Space: 16.2 MB (11.52%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    void dfs(TreeNode* root,
+             unordered_map<TreeNode*,TreeNode*>& mp) {
+        if(!root) return;
+        
+        if(root->left) mp[root->left]=root;
+        if(root->right) mp[root->right]=root;
+        
+        dfs(root->left,mp);
+        dfs(root->right,mp);
+    }
+
+
+    void findNodes(TreeNode* node,
+                   int k,
+                   int dist,
+                   vector<int>& res,
+                   unordered_map<TreeNode*,TreeNode*>& mp,
+                   unordered_map<TreeNode*,bool>& visited) {
+        
+        if(!node || visited[node]) return;
+        
+        visited[node] = true;
+        
+        if(dist == k){
+            res.push_back(node->val);
+            return;
+        }
+
+        findNodes(node->left,k,dist+1,res,mp,visited);
+        findNodes(node->right,k,dist+1,res,mp,visited);
+        
+        if(mp.find(node)!=mp.end())
+            findNodes(mp[node],k,dist+1,res,mp,visited);
+    }
+
+
+    vector<int> distanceK(TreeNode* root,
+                          TreeNode* target,
+                          int k) {
+        
+        unordered_map<TreeNode*,TreeNode*> mp;
+        dfs(root,mp);
+        
+        vector<int> res;
+        unordered_map<TreeNode*,bool> visited;
+        
+        findNodes(target,k,0,res,mp,visited);
+        
+        return res;
+    }
+};
