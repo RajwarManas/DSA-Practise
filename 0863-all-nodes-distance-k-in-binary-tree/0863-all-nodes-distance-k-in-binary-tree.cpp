ea1995cffaf9902dd123461d4ee843ff0cfc6b54
RajwarManas <manasrajwar.28@gmail.com>
2026-03-25 20:48:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -13,42 +13,26 @@ public:
     }
 
 
-    void findNodes(TreeNode* node,
-                   int k,
-                   int dist,
-                   vector<int>& res,
+    void findNodes(TreeNode* node,int k,int dist,vector<int>& res,
                    unordered_map<TreeNode*,TreeNode*>& mp,
                    unordered_map<TreeNode*,bool>& visited) {
-        
         if(!node || visited[node]) return;
-        
         visited[node] = true;
-        
         if(dist == k){
             res.push_back(node->val);
             return;
         }
-
         findNodes(node->left,k,dist+1,res,mp,visited);
-        findNodes(node->right,k,dist+1,res,mp,visited);
-        
+        findNodes(node->right,k,dist+1,res,mp,visited);   
         if(mp.find(node)!=mp.end())
             findNodes(mp[node],k,dist+1,res,mp,visited);
-    }
-
-
-    vector<int> distanceK(TreeNode* root,
-                          TreeNode* target,
-                          int k) {
-        
+        }
+    vector<int> distanceK(TreeNode* root,TreeNode* target,int k) {
         unordered_map<TreeNode*,TreeNode*> mp;
         dfs(root,mp);
-        
         vector<int> res;
         unordered_map<TreeNode*,bool> visited;
-        
         findNodes(target,k,0,res,mp,visited);
-        
         return res;
     }
 };
