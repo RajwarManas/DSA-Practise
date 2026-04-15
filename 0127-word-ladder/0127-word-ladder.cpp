c0fe9516c64a0184c782ab815e7a4aac476f178e
RajwarManas <manasrajwar.28@gmail.com>
2026-04-15 10:38:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        if (st.find(endWord) == st.end()) return 0;
         queue<string> q;
-        unordered_map<string,bool> um;
-        unordered_map<string,bool> vis;
-        int count=1,wl=beginWord.length();
-        bool possible=0,wordFound=0;
-        for(string& x:wordList) {
-            if(x==endWord) possible=1;
-            um[x]=1;
-            vis[x]=0;
-        }
-        if(!possible) return 0;
         q.push(beginWord);
-        vis[beginWord]=1;
-        while(!q.empty()) {
-            int size=q.size();
-            for(int i=0;i<size;i++) {
-                string word=q.front();
+        int count = 1;
+        int wl = beginWord.length();
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                string word = q.front();
                 q.pop();
-                for(int j=0;j<wl;j++) {
-                    string copy=word;
-                    for(int k=0;k<26;k++) {
-                        copy[j]='a'+k;
-                        if(um.find(copy)!=um.end()&&vis[copy]==0) {
-                            q.push(copy);
-                            vis[copy]=1;
-                            if(copy==endWord) return count+1;
+                for (int j = 0; j < wl; j++) {
+                    string temp = word;
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        if (ch == word[j]) continue;
+                        temp[j] = ch;
+                        if (temp == endWord) return count + 1;
+                        if (st.find(temp) != st.end()) {
+                            q.push(temp);
+                            st.erase(temp);
                         }
                     }
                 }
@@ -34,5 +28,5 @@ public:
             count++;
         }
         return 0;
-    }
+    } 
 };
