dfb65893ad0039f5b2da2aabfb8189cd5ba42c56
RajwarManas <manasrajwar.28@gmail.com>
2026-08-25 11:37:04 +0530

    Time: 80 ms (38.45%), Space: 24.1 MB (15.79%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-        int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        if (st.find(endWord) == st.end()) return 0;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if (beginWord==endWord) return 1;
         queue<string> q;
+        unordered_map<string,bool> um;
+        unordered_map<string,bool> vis;
+        int count=1,wl=beginWord.length();
+        bool possible=0;
+        for(string& x:wordList) {
+            if(x==endWord) possible=1;
+            um[x]=1;
+            vis[x]=0;
+        }
+        if (!possible) return 0;
         q.push(beginWord);
-        int count = 1;
-        int wl = beginWord.length();
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                string word = q.front();
+        vis[beginWord]=1;
+        while(!q.empty()) {
+            int size=q.size();
+            for(int i=0;i<size;i++) {
+                string word=q.front();
                 q.pop();
-                for (int j = 0; j < wl; j++) {
-                    string temp = word;
-                    for (char ch = 'a'; ch <= 'z'; ch++) {
-                        if (ch == word[j]) continue;
-                        temp[j] = ch;
-                        if (temp == endWord) return count + 1;
-                        if (st.find(temp) != st.end()) {
-                            q.push(temp);
-                            st.erase(temp);
+                for(int j=0;j<wl;j++) {
+                    string copy=word;
+                    for(int k=0;k<26;k++) {
+                        copy[j]= 'a' + k;
+                        if (um.find(copy)!=um.end() && vis[copy]==0) {
+                            vis[copy]=1;
+                            q.push(copy);
+                            if (copy==endWord) return count+1;
                         }
                     }
                 }
@@ -28,5 +35,9 @@ public:
             count++;
         }
         return 0;
-    } 
+    }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
